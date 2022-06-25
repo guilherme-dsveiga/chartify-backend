d8fb65f249d287d5d8535d03017ede715f3846ed
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2022-06-25 08:03:22 -0300

    adding dotenv

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const SpotifyWebApi = require("spotify-web-api-node");
-
 const app = express();
 
 app.use(cors());
