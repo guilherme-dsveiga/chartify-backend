e7c6a0dfd64fc2e44c9e23312e72579063dc3c8c
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2022-07-03 19:12:29 -0300

    changing url

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 3001);
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refresh_token;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
+    redirectUri: "chartify-nu.vercel.app",
     clientId: "fca3904abf904521b90b32ea5da9aed9",
     clientSecret: "7ad92d54797f491b8f3248fce9cacad7",
     refreshToken,
@@ -35,7 +35,7 @@ app.post("/refresh", (req, res) => {
 app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
+    redirectUri: "chartify-nu.vercel.app",
     clientId: "fca3904abf904521b90b32ea5da9aed9",
     clientSecret: "7ad92d54797f491b8f3248fce9cacad7",
   });
